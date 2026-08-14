ae953ac782dc4b72e746fbd81759afedbc3f7f4c
Nicolas Vera Vizcaya <nicolas.vera@estudiantesunibague.edu.co>
2026-08-14 09:46:43 -0500

    Elimina main.py de funcionalidad-2

--- main.py
+++
@@ -1,7 +0,0 @@
-nombre = input("¿Cuál es tu nombre? ")
-edad = input("¿Cuántos años tienes? ")
-carrera = input("¿Qué carrera estudias? ")
-
-print(f"\nHola, {nombre}!")
-print(f"Tienes {edad} años.")
-print(f"Estudias {carrera}.")
